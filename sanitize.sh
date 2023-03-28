847f47c23e88a423b05b25d87a7152d096a0c264
Jeonghyeon Kim <powergee101@gmail.com>
2023-03-28 08:50:44 +0000

    Sanitize with a release build

--- sanitize.sh
+++ sanitize.sh
@@ -3,5 +3,5 @@
 set -e
 for i in {1..1000}; do
     echo ${i}
-    RUSTFLAGS="-Z sanitizer=address" cargo test --target x86_64-unknown-linux-gnu -- --nocapture
+    RUSTFLAGS="-Z sanitizer=address" cargo test --release --target x86_64-unknown-linux-gnu -- --nocapture
 done
